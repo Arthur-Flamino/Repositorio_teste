b6ac3ffdbed4e8a7fbf3ada6ec89b59316aaf1ec
Auto Logon <autologon@CSED.LAB>
2024-05-23 09:57:55 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Repositorio_teste
-teste
+# projeto-final-manha
--- demo/src/main/java/to/do/list/Main.java
+++
@@ -1,7 +0,0 @@
-package to.do.list;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
